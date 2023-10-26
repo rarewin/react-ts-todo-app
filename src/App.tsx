5307a288a28c17ff4a298a24ff2755f699a6462a
Katsuki Kobayashi <rare@tirasweel.org>
2023-10-27 01:05:56 +0900

    アイテムを並べる

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,34 @@
 import React from "react";
+import { Dispatch, SetStateAction } from "react";
 import "./App.css";
 
-function Todo(): JSX.Element | null {
-  return null;
+interface TodoItemInterface {
+  key: string;
+  text: string;
+  done: boolean;
+}
+
+function Todo(): JSX.Element {
+  const getKey = () => Math.random().toString(32).substring(2);
+  const [items, setItems]: [Array<TodoItemInterface>, Dispatch<SetStateAction<Array<TodoItemInterface>>>] =
+    React.useState<Array<TodoItemInterface>>([
+      { key: getKey(), text: "Learn JavaScript", done: false },
+      { key: getKey(), text: "Learn React", done: false },
+      { key: getKey(), text: "Get some good sleep", done: false },
+    ]);
+
+  return (
+    <div className="panel">
+      <div className="panel-heading">⚛️ React ToDo</div>
+      {items.map((item) => (
+        <label className="panel-block">
+          <input type="checkbox" />
+          {item.text}
+        </label>
+      ))}
+      <div className="panel-block">{items.length} items</div>
+    </div>
+  );
 }
 
 function App(): JSX.Element {
