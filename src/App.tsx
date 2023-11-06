4efff5d783238d59c94dd450fca40a4efc0a5910
Katsuki Kobayashi <rare@tirasweel.org>
2023-11-07 00:15:34 +0900

    フィルター機能追加

--- src/App.tsx
+++ src/App.tsx
@@ -58,6 +58,27 @@ function Input({ onAdd }: { onAdd: (t: string) => void }): JSX.Element {
   );
 }
 
+function Filter({ value, onChange }: { value: string; onChange: (key: string) => void }): JSX.Element {
+  const handleClick = (key: string, event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    event.preventDefault();
+    onChange(key);
+  };
+
+  return (
+    <div className="panel-tabs">
+      <a href="#" onClick={handleClick.bind(null, "ALL")} className={classNames({ "is-active": value === "ALL" })}>
+        All
+      </a>
+      <a href="#" onClick={handleClick.bind(null, "TODO")} className={classNames({ "is-active": value === "TODO" })}>
+        ToDo
+      </a>
+      <a href="#" onClick={handleClick.bind(null, "DONE")} className={classNames({ "is-active": value === "DONE" })}>
+        Done
+      </a>
+    </div>
+  );
+}
+
 function Todo(): JSX.Element {
   const getKey = () => Math.random().toString(32).substring(2);
   const [items, setItems]: [Array<TodoItemInterface>, Dispatch<SetStateAction<Array<TodoItemInterface>>>] =
@@ -66,6 +87,14 @@ function Todo(): JSX.Element {
       { key: getKey(), text: "Learn React", done: false },
       { key: getKey(), text: "Get some good sleep", done: false },
     ]);
+  const [filter, setFilter] = React.useState("ALL");
+
+  const displayItems = items.filter((item) => {
+    if (filter === "ALL") return true;
+    if (filter === "TODO") return !item.done;
+    if (filter === "DONE") return item.done;
+  });
+
   const handleCheck = (checked: TodoItemInterface) => {
     const newItems = items.map((item) => {
       if (item.key === checked.key) {
@@ -78,15 +107,19 @@ function Todo(): JSX.Element {
   const handleAdd = (text: string) => {
     setItems([...items, { key: getKey(), text, done: false }]);
   };
+  const handleFilterChange = (value: string) => {
+    setFilter(value);
+  };
 
   return (
     <div className="panel">
       <div className="panel-heading">⚛️ React ToDo</div>
       <Input onAdd={handleAdd} />
-      {items.map((item) => (
+      <Filter value={filter} onChange={handleFilterChange} />
+      {displayItems.map((item) => (
         <TodoItem key={item.key} item={item} onCheck={handleCheck} />
       ))}
-      <div className="panel-block">{items.length} items</div>
+      <div className="panel-block">{displayItems.length} items</div>
     </div>
   );
 }
