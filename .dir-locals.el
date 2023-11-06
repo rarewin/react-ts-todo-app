500ac09d660cfc65c73a768fcd3a2587f1c19615
Katsuki Kobayashi <rare@tirasweel.org>
2023-11-06 23:22:58 +0900

    新規作成まで

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,7 +1,8 @@
-((web-mode .
+((nil . ((indent-tabs-mode . nil)))
+ (web-mode .
 	   (
 	    (web-mode-markup-indent-offset . 2)
 	    (web-mode-css-indent-offset    . 2)
 	    (web-mode-code-indent-offset   . 2)
-	    (eval add-hook 'before-save-hook #'clang-format-buffer nil t)
+	    ()
 	 )))
--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import classNames from "classnames";
 import { Dispatch, SetStateAction } from "react";
 import "./App.css";
 
@@ -6,6 +7,55 @@ interface TodoItemInterface {
   key: string;
   text: string;
   done: boolean;
+}
+
+function TodoItem({
+  item,
+  onCheck,
+}: {
+  item: TodoItemInterface;
+  onCheck: (item: TodoItemInterface) => void;
+}): JSX.Element {
+  const handleChange = () => {
+    onCheck(item);
+  };
+
+  return (
+    <label className="panel-block">
+      <input type="checkbox" checked={item.done} onChange={handleChange} />
+      <span
+        className={classNames({
+          "has-text-grey-light": item.done,
+        })}
+      >
+        {item.text}
+      </span>
+    </label>
+  );
+}
+
+function Input({ onAdd }: { onAdd: (t: string) => void }): JSX.Element {
+  const [text, setText] = React.useState("");
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => setText(event.target.value);
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      onAdd(text);
+      setText("");
+    }
+  };
+
+  return (
+    <div className="panel-block">
+      <input
+        className="input"
+        type="text"
+        placeholder="Enter to add"
+        value={text}
+        onChange={handleChange}
+        onKeyDown={handleKeyDown}
+      />
+    </div>
+  );
 }
 
 function Todo(): JSX.Element {
@@ -16,15 +66,25 @@ function Todo(): JSX.Element {
       { key: getKey(), text: "Learn React", done: false },
       { key: getKey(), text: "Get some good sleep", done: false },
     ]);
+  const handleCheck = (checked: TodoItemInterface) => {
+    const newItems = items.map((item) => {
+      if (item.key === checked.key) {
+        item.done = !item.done;
+      }
+      return item;
+    });
+    setItems(newItems);
+  };
+  const handleAdd = (text: string) => {
+    setItems([...items, { key: getKey(), text, done: false }]);
+  };
 
   return (
     <div className="panel">
       <div className="panel-heading">⚛️ React ToDo</div>
+      <Input onAdd={handleAdd} />
       {items.map((item) => (
-        <label className="panel-block">
-          <input type="checkbox" />
-          {item.text}
-        </label>
+        <TodoItem key={item.key} item={item} onCheck={handleCheck} />
       ))}
       <div className="panel-block">{items.length} items</div>
     </div>
